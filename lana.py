40fb194417c510cbdd17b8a819ea0d3829d57aec
faithdelrey <47580357+faithdelrey@users.noreply.github.com>
2019-02-13 13:21:57 +1300

    Add files via upload

---
+++ lana.py
@@ -0,0 +1,9 @@
+print("I love Lana Del Rey more than anything else on this planet")
+print("Her voice is so powerful and full of emotion")
+print("Her lyrics reveal so much about her past and show how far she has come. She is very real about her struggles and her reality which means many people can relate to her music and feel the emotion with her.")
+print("She has such a kind soul, she treats her fans so well and has strong views on the current state of the world")
+loveLana= input("Do you also love Lana?")
+if loveLana != "yes":
+    print("We are not friends any more.")
+else:
+    print("I love you <3")
